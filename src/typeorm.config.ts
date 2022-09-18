a66f1e9bf70272cba46ce92616677b5738a0a8a0
Farid <faridforward222@gmail.com>
2022-09-18 18:09:26 +0430

    testing new remote database

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,18 +1,19 @@
-import path from "path";
-import { DataSource } from "typeorm";
-import { Post } from "./entities/Post";
-import { Updoot } from "./entities/Updoot";
-import { User } from "./entities/User";
+import path from 'path';
+import { DataSource } from 'typeorm';
+import { Post } from './entities/Post';
+import { Updoot } from './entities/Updoot';
+import { User } from './entities/User';
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
+  type: 'postgres',
+  host: 'localhost',
   port: 5432,
-  username: "postgres",
-  password: "admin",
-  database: "reddit2",
+  username: 'postgres',
+  password: 'admin',
+  database: 'reddit2',
   entities: [User, Post, Updoot],
   synchronize: true,
-  migrations: [path.join(__dirname, "./migrations/*")],
+  migrations: [path.join(__dirname, './migrations/*')],
   logging: true,
+  // url: 'postgres://gtaflbda:7g88kK-fQHNPIVVTHmkU26Ca2AXBHJpG@jelani.db.elephantsql.com/gtaflbda',
 });
